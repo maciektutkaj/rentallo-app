94bcad13483cc468dff7d4d8f57605fc151449bd
Uzytkownik <uzytkownik@kursGIT.pl>
2023-07-25 20:36:31 +0200

    [add] - record car DTO

---
+++ src/main/java/com/sda/rentalloapp/dto/CarDto.java
@@ -0,0 +1,30 @@
+package com.sda.rentalloapp.dto;
+
+import com.sda.rentalloapp.domain.Pictures;
+import com.sda.rentalloapp.domain.enumeration.BodyType;
+import com.sda.rentalloapp.domain.enumeration.EngineType;
+import com.sda.rentalloapp.domain.enumeration.FuelType;
+
+public record CarDto(
+        Long id,
+        String model,
+        String brand,
+        FuelType fuelType,
+        EngineType engineType,
+        BodyType bodyType,
+        int numberOfSeats,
+
+        int trunkCapacityInLitres,
+
+        String consumptionPer100km,
+
+        String vin,
+
+        int pricePerDayInPolishGrosz,
+
+        boolean availability,
+
+        int rangeInKm
+        )
+{}
+
