b7d0e42b15dc668ee9603f61ef70463c27e380f2
Uzytkownik <uzytkownik@kursGIT.pl>
2023-07-26 20:21:53 +0200

    [change] - RestControllerAdvice created

---
+++ src/main/java/com/sda/rentalloapp/dto/ResponseDto.java
@@ -0,0 +1,12 @@
+package com.sda.rentalloapp.dto;
+
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+@Builder
+public record ResponseDto(LocalDateTime timestamp,
+                          int status,
+                          String error,
+                          String message,
+                          String path) {
+}
