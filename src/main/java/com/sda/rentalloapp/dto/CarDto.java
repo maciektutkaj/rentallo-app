530608ca3046cd47e1996f7523ed0579b2e0a562
Uzytkownik <uzytkownik@kursGIT.pl>
2023-07-26 18:07:48 +0200

    [change] - car mapper to fromEntityToDto implemented

--- src/main/java/com/sda/rentalloapp/dto/CarDto.java
+++ src/main/java/com/sda/rentalloapp/dto/CarDto.java
@@ -1,10 +1,11 @@
 package com.sda.rentalloapp.dto;
 
-import com.sda.rentalloapp.domain.Pictures;
 import com.sda.rentalloapp.domain.enumeration.BodyType;
 import com.sda.rentalloapp.domain.enumeration.EngineType;
 import com.sda.rentalloapp.domain.enumeration.FuelType;
+import lombok.Builder;
 
+@Builder
 public record CarDto(
         Long id,
         String model,
--- src/main/java/com/sda/rentalloapp/mapper/CarMapper.java
+++ src/main/java/com/sda/rentalloapp/mapper/CarMapper.java
@@ -2,13 +2,29 @@ package com.sda.rentalloapp.mapper;
 
 import com.sda.rentalloapp.domain.Car;
 import com.sda.rentalloapp.dto.CarDto;
+import com.sda.rentalloapp.dto.PicturesDto;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CarMapper implements Mapper<Car, CarDto>{
     @Override
     public CarDto fromEntityToDto(Car entity) {
-        return null;
+        return CarDto.builder()
+                .id(entity.getId())
+                .model(entity.getModel())
+                .brand(entity.getBrand())
+                .fuelType(entity.getFuelType())
+                .engineType(entity.getEngineType())
+                .bodyType(entity.getBodyType())
+                .numberOfSeats(entity.getNumberOfSeats())
+                .trunkCapacityInLitres(entity.getTrunkCapacityInLitres())
+                .consumptionPer100km(entity.getConsumptionPer100km())
+                .vin(entity.getVin())
+                .pricePerDayInPolishGrosz(entity.getPricePerDayInPolishGrosz())
+                .availability(entity.isAvailability())
+                .rangeInKm(entity.getRangeInKm())
+                .pictures(new PicturesDto(entity.getPictures().getMainPictureUrl(),entity.getPictures().getPicturesUrls()))
+                .build();
     }
 
     @Override
