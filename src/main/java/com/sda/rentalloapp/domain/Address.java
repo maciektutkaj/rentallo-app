c3b70702cc99011e0690a465aa7effa007076cbc
Uzytkownik <uzytkownik@kursGIT.pl>
2023-08-07 18:31:17 +0200

    [add] - Client and Address class created

---
+++ src/main/java/com/sda/rentalloapp/domain/Address.java
@@ -0,0 +1,12 @@
+package com.sda.rentalloapp.domain;
+
+public class Address {
+
+    String street;
+    String zipCode;
+    String city;
+    String houseNumber;
+    String voivodeship;
+    String country;
+
+}
