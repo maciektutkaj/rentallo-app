de008ecfa96f0f3e0f3fdb5e3fa205a9626a99e0
Uzytkownik <uzytkownik@kursGIT.pl>
2023-08-22 19:20:42 +0200

    [refactor] - routing table

--- frontend/src/app/components/app.module.ts
+++ frontend/src/app/components/app.module.ts
@@ -1,23 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app/app.component';
-import { CarsComponent } from './cars/cars.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app/app.component';
+import {CarsComponent} from './cars/cars.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
-import { TopBarComponent } from "./top-bar/top-bar.component";
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import {TopBarComponent} from "./top-bar/top-bar.component";
+import {NavigationBarComponent} from './navigation-bar/navigation-bar.component';
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
-import { ClientsComponent } from './clients/clients.component';
-import { AboutComponent } from './about/about.component';
-import { RentsHistoryComponent } from './rents-history/rents-history.component';
-import {RouterModule} from "@angular/router";
+import {ClientsComponent} from './clients/clients.component';
+import {AboutComponent} from './about/about.component';
+import {RentsHistoryComponent} from './rents-history/rents-history.component';
+import {Route, RouterModule, Routes} from "@angular/router";
 import {aboutUrl, carsUrl, clientsUrl, homePageUrl, myRentsHistoryUrl, notFoundPageUrl} from "../models/urls";
-import { HomePageComponent } from './home-page/home-page.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
+const routingTable: Routes = [
+    {path: homePageUrl, component: HomePageComponent},
+    {path: myRentsHistoryUrl, component: RentsHistoryComponent},
+    {path: carsUrl, component: CarsComponent},
+    {path: clientsUrl, component: ClientsComponent},
+    {path: aboutUrl, component: AboutComponent},
+    {path: notFoundPageUrl, component: NotFoundComponent}
+  ]
 @NgModule({
   declarations: [
     AppComponent,
@@ -37,16 +45,11 @@ import { NotFoundComponent } from './not-found/not-found.component';
     MatCardModule,
     MatButtonModule,
     MatButtonToggleModule,
-    RouterModule.forRoot([
-      {path:homePageUrl, component: HomePageComponent},
-      {path: myRentsHistoryUrl, component:RentsHistoryComponent},
-      {path: carsUrl, component:CarsComponent},
-      {path: clientsUrl, component:ClientsComponent},
-      {path: aboutUrl, component: AboutComponent},
-      {path: notFoundPageUrl, component:NotFoundComponent}
-    ])
+    RouterModule.forRoot(routingTable)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
