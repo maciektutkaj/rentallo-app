cc2550ef7021bc5c4496b57a222d93212995ec7b
Uzytkownik <uzytkownik@kursGIT.pl>
2023-08-22 19:15:23 +0200

    [change] - navigation with links

--- frontend/src/app/components/navigation-bar/navigation-bar.component.ts
+++ frontend/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {aboutUrl, carsUrl, clientsUrl, homePageUrl, myRentsHistoryUrl} from "../../models/urls";
 
 @Component({
   selector: 'app-navigation-bar',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class NavigationBarComponent {
 
+  protected readonly homePageUrl = homePageUrl;
+  protected readonly myRentsHistoryUrl = myRentsHistoryUrl;
+  protected readonly carsUrl = carsUrl;
+  protected readonly clientsUrl = clientsUrl;
+  protected readonly aboutUrl = aboutUrl;
 }
