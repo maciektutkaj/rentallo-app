6d4921fca3f802f17e31204c61bd523f7c0c93dc
Uzytkownik <uzytkownik@kursGIT.pl>
2023-08-28 20:15:11 +0200

    [change] - ui config completed

--- backend-service/src/main/java/com/sda/rentalloapp/controller/UiConfigController.java
+++ backend-service/src/main/java/com/sda/rentalloapp/controller/UiConfigController.java
@@ -1,11 +1,44 @@
 package com.sda.rentalloapp.controller;
 
+import com.sda.rentalloapp.domain.enumeration.BodyType;
+import com.sda.rentalloapp.domain.enumeration.EngineType;
+import com.sda.rentalloapp.domain.enumeration.FuelType;
+import com.sda.rentalloapp.dto.ConfigItem;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @Slf4j
 @RequestMapping("/api/config")
 public class UiConfigController {
+
+    @GetMapping("/fuel-types")
+    public List<ConfigItem<String, String>> getFuelTypes() {
+// call values on enum
+        return Arrays.stream(FuelType.values())
+                .map(fuelType ->
+                        new ConfigItem<>(fuelType.name(), fuelType.fuelDescription()))
+                .toList();
+    }
+
+    @GetMapping("/body-types")
+    public List<ConfigItem<String, String>> getCarBodyTypes() {
+        return Arrays.stream(BodyType.values())
+                .map(bodyType ->
+                        new ConfigItem<>(bodyType.name(),
+                                bodyType.description()))
+                .toList();
+    }
+    @GetMapping("/engine-type")
+    public List<ConfigItem<String,String>> getCarEngineType() {
+        return Arrays.stream(EngineType.values())
+                .map(engineType ->
+                        new ConfigItem<>(engineType.name(),engineType.description()))
+                .toList();
+    }
 }
--- backend-service/src/main/java/com/sda/rentalloapp/domain/enumeration/EngineType.java
+++ backend-service/src/main/java/com/sda/rentalloapp/domain/enumeration/EngineType.java
@@ -1,10 +1,18 @@
 package com.sda.rentalloapp.domain.enumeration;
 
 public enum EngineType {
-    ELECTRIC_MOTOR,
-    V12,
-    V10,
-    V8,
-    V6,
-    R4
+    ELECTRIC_MOTOR ("Electric motor"),
+    V12 ("V12"),
+    V10 ("V10"),
+    V8 ("V8"),
+    V6 ("V6"),
+    R4 ("R4");
+
+    private final String description;
+    EngineType(String description) {
+        this.description = description;
+    }
+    public String description() {
+        return description;
+    }
 }
