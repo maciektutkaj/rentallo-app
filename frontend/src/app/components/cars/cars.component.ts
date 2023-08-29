61d9415a56ac1b1763f265e1e3845de701352439
Uzytkownik <uzytkownik@kursGIT.pl>
2023-08-29 19:41:38 +0200

    [change] - sendCars form still in progress

--- frontend/src/app/components/cars/cars.component.ts
+++ frontend/src/app/components/cars/cars.component.ts
@@ -1,81 +1,79 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {CarService} from "../../services/car/car.service";
 import {Car} from "../../models/car";
 import {FormControl, FormGroup} from "@angular/forms";
-
+import {MatButton} from "@angular/material/button";
+import {delay} from "rxjs";
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
   styleUrls: ['./cars.component.css']
 })
 export class CarsComponent implements OnInit {
-
+  @ViewChild('submitButton') submitButton!: MatButton
   cars: Array<Car> = []
+  // TODO: with explicit FormGroup
   carForm = new FormGroup({
-    id:new FormControl(null),
-    model:new FormControl(''),
+    model: new FormControl(''),
     brand: new FormControl(''),
     fuelType: new FormControl(''),
     engineType: new FormControl(''),
     bodyType: new FormControl(''),
-    numberOfSeat: new FormControl(4),
+    numberOfSeats: new FormControl(4),
     trunkCapacityInLitres: new FormControl(200),
-    combustionPer100km: new FormControl(''),
+    combustionPer100Km: new FormControl(''),
+    bodySerialNumber: new FormControl(''),
     pricePerDayInPolishGrosz: new FormControl(0),
-    availability: new FormControl(true),
+    available: new FormControl(true),
     rangeInKm: new FormControl(300),
     pictures: new FormGroup({
       mainPictureUrl: new FormControl(''),
-      picturesUrl: new FormControl(Array<string>)
-    }),
+      picturesUrls: new FormControl(new Array<string>())
+    })
   })
-
   value: any;
-  get pictures(){
-    return this.carForm.controls.pictures.controls.mainPictureUrl
-  }
-  get rangeInKm(){
+  get rangeInKm() {
     return this.carForm.controls.rangeInKm
   }
-
-  get availability(){
-    return this.carForm.controls.availability
+  get available() {
+    return this.carForm.controls.available
   }
-  get pricePerDayInPolishGrosz(){
+  get pricePerDayInPolishGrosz() {
     return this.carForm.controls.pricePerDayInPolishGrosz
-}
-  get combustionPer100km(){
-    return this.carForm.controls.combustionPer100km
   }
-  get trunkCapacityInLitres(){
+  get bodySerialNumber() {
+    return this.carForm.controls.bodySerialNumber
+  }
+  get combustionPer100Km() {
+    return this.carForm.controls.combustionPer100Km
+  }
+  get trunkCapacityInLitres() {
     return this.carForm.controls.trunkCapacityInLitres
   }
-
-  get numberOfSeat(){
-    return this.carForm.controls.numberOfSeat
+  get numberOfSeats() {
+    return this.carForm.controls.numberOfSeats
   }
-  get bodyType(){
+  get bodyType() {
     return this.carForm.controls.bodyType
   }
-  get engineType(){
+  get engineType() {
     return this.carForm.controls.engineType
   }
-
-  get fuelType(){
+  get fuelType() {
     return this.carForm.controls.fuelType
   }
-  get brand(){
-    return this.carForm.controls.brand
-  }
-
   get model() {
     return this.carForm.controls.model
   }
-
+  get brand() {
+    return this.carForm.controls.brand
+  }
   constructor(private carService: CarService) {
   }
-
   ngOnInit(): void {
+    this.getDataFromServer();
+  }
+  private getDataFromServer() {
     this.carService
       .getCars()
       .subscribe(carsFromServer => {
@@ -83,11 +81,22 @@ export class CarsComponent implements OnInit {
         this.cars = carsFromServer
       })
   }
-
   sendCar() {
+    this.submitButton.disabled = true
     console.log("data submitted")
     this.value = this.carForm.value
-    //this.carService.sendCar(this.carForm.value as Car)
+    this.carService.sendCar(this.carForm.value as Car)
+      .pipe(delay(1000))
+      .subscribe(response => {
+          console.log("Car created")
+          this.getDataFromServer()
+          // enable button
+          this.submitButton.disabled = false
+          this.carForm.reset()
+        },
+        error => {
+          console.log("Error")
+          this.submitButton.disabled = false
+        })
   }
-
 }
